37084a92771fff2ff4f17361325782506df2bbbe
ydnamjs <ydnamjs@udel.edu>
2024-08-08 04:16:01 -0400

    Checkpoint Commit: Added basic api for sending an image and getting the preprocessed and output image though it has not been tested yet

--- src/main.py
+++ src/main.py
@@ -1,21 +1,71 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile
+from pydantic import BaseModel
+from PIL import Image
+from io import BytesIO
+
+from .process_image import preprocess_image, predict_image, process_predictions, image_to_base64
+
+# new_session_id = 0
+
+class ImageModel(BaseModel):
+    preprocessed_image: str
+    output_image: str
+    total: int
+
+# active_sessions = {}
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def root():
+#     return {"Hello": "World"}
 
-items = []
+# items = []
 
-@app.post("/items")
-def create_item(item: str):
-    items.append(item)
-    return items
+# @app.post("/items")
+# def create_item(item: str):
+#     items.append(item)
+#     return items
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
-    if item_id < len(items) and item_id >= 0:
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+# @app.get("/items/{item_id}")
+# def get_item(item_id: int) -> str:
+#     if item_id < len(items) and item_id >= 0:
+#         return items[item_id]
+#     else:
+#         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+# @app.get("/start-new-session/")
+# def start_new_session():
+#     session_id = new_session_id
+#     new_session_id = new_session_id + 1
+#     return session_id
+
+@app.post("/post-image/", response_model=ImageModel)
+async def process_frame(image: UploadFile, canny_threshold_1: int, canny_threshold_2):
+
+    if image.content_type not in ["image/png"]:
+        raise HTTPException(status_code=400, detail="Invalid file type. Only PNG files are allowed.")
+
+    image = Image.open(BytesIO(image))
+    preprocessed_image_array = preprocess_image(image, canny_threshold_1, canny_threshold_2)
+    boxes, scores, predictions = predict_image(preprocessed_image_array)
+    outputImage, predicted_total = process_predictions(image, boxes, scores, predictions, .65)
+
+    return ImageModel(
+        preprocessed_image=image_to_base64(Image.fromarray(preprocessed_image_array)), 
+        output_image=image_to_base64(outputImage), 
+        total=predicted_total
+    )
+
+# @app.get("/get-processed-image")
+# async def get_processed_image(id: int):
+#     if id not in active_sessions:
+#         raise HTTPException(status_code=404, detail="Image not found")
+    
+#     image = active_sessions[id]
+    
+#     img_byte_arr = BytesIO()
+#     image.save(img_byte_arr, format='PNG')
+#     img_byte_arr.seek(0)
+    
+#     return responses.FileResponse(img_byte_arr, media_type="image/png")
---
+++ src/process_image.py
@@ -0,0 +1,98 @@
+from torchvision import transforms
+import torchvision.models as models
+import bbox_visualizer as bbv
+from io import BytesIO
+import base64
+import torch
+import numpy
+import PIL
+import cv2
+
+
+NUM_CLASSES = 6
+DETECTION_STATE_PATH = "../models/detector.pth"
+CLASSIFIER_STATE_PATH = "../models/classifier.pth"
+BBOX_COLOR = (255, 255, 255)
+LABEL_BG_COLOR = (0, 0, 0)
+LABEL_TEXT_COLOR = (255, 255, 255)
+PAD_COLOR = (0, 0, 0)
+PAD_AMT = 25
+
+detection_model = models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(weights=None, num_classes=2)
+detection_model.load_state_dict(torch.load(DETECTION_STATE_PATH))
+detection_model.eval()
+
+classification_model = models.mobilenet_v3_small(weights=None)
+num_features = classification_model.classifier[3].in_features
+classification_model.classifier[3] = torch.nn.Linear(num_features, NUM_CLASSES)
+classification_model.load_state_dict(torch.load(CLASSIFIER_STATE_PATH))
+
+transform = transforms.v2.Compose([
+    transforms.v2.ToDtype(torch.float32, scale=True),
+    transforms.v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+tensorify = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+def preprocess_image(image, threshold1, threshold2):
+    imageArray = numpy.array(image)
+    
+    processed_image_array = cv2.resize(imageArray, (512, 512), interpolation=cv2.INTER_LANCZOS4)
+    processed_image_array = cv2.GaussianBlur(processed_image_array, (5, 5), 0)
+    processed_image_array = cv2.Canny(processed_image_array, threshold1=threshold1, threshold2=threshold2)
+
+    return processed_image_array
+
+def predict_image(preprocessed_image):
+
+    imageTensor = tensorify(preprocessed_image)
+    imageTensor = transform(imageTensor)
+
+    with torch.no_grad():
+        prediction = detection_model([imageTensor])
+
+    scores = numpy.array(prediction[0]['scores'])
+    boxes = numpy.array(prediction[0]['boxes']).astype(numpy.int32)
+
+    result = cv2.imread("cropped_image.jpg")
+    result = cv2.resize(result, (512, 512), interpolation=cv2.INTER_LANCZOS4)
+    predictions = []
+    for box in boxes:
+            
+        x1, y1, x2, y2 = box
+
+        subImageArray = preprocessed_image[int(y1):int(y2), int(x1):int(x2)]
+        subImageArray = cv2.resize(subImageArray, (125, 125), interpolation=cv2.INTER_LANCZOS4)
+        subImageTensor = tensorify(subImageArray)
+        subImageTensor = transform(subImageTensor)
+        subImageTensor = torch.unsqueeze(subImageTensor, 0)
+        output = classification_model(subImageTensor)
+
+        predicted_class = torch.argmax(output, dim=1)
+        predictions.append(predicted_class.item())
+    
+    return boxes, scores, predictions
+
+def process_predictions(image, boxes, scores, predictions, scoreThreshold):
+    
+    result = cv2.copyMakeBorder(numpy.array(image), PAD_AMT, PAD_AMT, PAD_AMT, PAD_AMT, cv2.BORDER_CONSTANT, value=PAD_COLOR)
+    predicted_total = 0
+    for i in range(0, len(boxes)):
+
+        if scores[i] < scoreThreshold:
+            continue
+
+        label = str(predictions[i] + 1)
+        predicted_total = predicted_total + predictions[i] + 1
+
+        result = bbv.draw_rectangle(result, boxes[i], BBOX_COLOR, thickness=1)
+        result = bbv.add_label(result, label, boxes[i], text_bg_color=LABEL_BG_COLOR, text_color=LABEL_TEXT_COLOR)
+
+    return PIL.Image.fromarray(result), predicted_total
+
+def image_to_base64(image: PIL.Image.Image) -> str:
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode('utf-8')
